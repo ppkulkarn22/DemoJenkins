af09cd3c1511aba032047a4736eb4951d8cc19b2
Prashant <prashant.pralhadrao@gmail.com>
2020-05-11 16:48:01 +0530

    changed something

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
 	public static void main(String[] args)
 		{
-			System.out.println("Practice update - Hello World!!!");
+			System.out.println("Good to know its working - Hello World!!!");
 		}
 }
